37bf122eda127713d53b9478316f05ce61c3a76a
Anthony Paredes-Bautista <anthonyparedesb0@gmail.com>
2025-09-16 19:51:19 -0400

    Initial Commit UNIT 1 folder created

---
+++ UNIT 1/session1.py
@@ -0,0 +1,30 @@
+# Breakout Problems Session 1
+# Problem Set 1:
+def welcome():
+	print("Welcome to The Hundred Acre Wood!")
+
+def greeting(name):
+	print("Welcome to The Hundred Acre Wood " + name + "! My name is Christopher Robin.")
+
+# Tests
+#welcome()
+#greeting("Anthony")
+
+
+
+
+# Problem Set 2:
+def batman():
+    print("I am vengeance. I am the night. I am Batman!")
+
+
+
+
+
+
+
+
+
+
+# Tests
+#batman()
