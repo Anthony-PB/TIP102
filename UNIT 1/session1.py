3eb5272fa2017c2c8bf354510da380b73dda6b9e
Anthony Paredes-Bautista <anthonyparedesb0@gmail.com>
2025-09-16 20:59:04 -0400

    Update session1.py

--- UNIT 1/session1.py
+++ UNIT 1/session1.py
@@ -6,6 +6,18 @@ def welcome():
 def greeting(name):
 	print("Welcome to The Hundred Acre Wood " + name + "! My name is Christopher Robin.")
 
+def print_catchphrase(character):
+	if character == "Pooh":
+        print("Oh bother!")
+    else if character == "Tigger":
+        print("TTFN: Ta-ta for now!")
+    else ifcharacter == "Eeyore":
+        print("Thanks for noticing me.")
+    else if character == "Christopher Robin":
+        print("Silly old bear.")
+    else:
+        print("Sorry! I don't know " + character "+'s catchphrase!")
+
 # Tests
 #welcome()
 #greeting("Anthony")
