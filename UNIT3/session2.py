dba65221d5b3a5f1208107502efd8ab9ba897b96
Anthony Paredes-Bautista <ap2357@cornell.edu>
2025-10-02 21:55:02 -0400

    2 questions done the other one I left on live share

--- UNIT3/session2.py
+++ UNIT3/session2.py
@@ -55,3 +55,27 @@ class SparseVector:
             if(i < vec1.size):
                 res += current * vec1.get(i)
         return res
+
+# Problem 1
+"""
+You are managing the budget for a global expedition, where the cost of supplies is represented by an integer array costs, where costs[i] is the cost of the ith supply item.
+
+There is a special discount available during the expedition. If you purchase the ith item, you will receive a discount equivalent to costs[j], where j is the minimum index such that j > i and costs[j] <= costs[i]. If no such j exists, you will not receive any discount.
+
+Return an integer array final_costs where final_costs[i] is the final cost you will pay for the ith supply item, considering the special discount.
+"""
+
+def final_supply_costs(costs):
+
+  n = len(costs)
+  final_costs = list(costs)
+  stack = []
+
+  for i, current_cost in enumerate(costs):
+    while stack and costs[stack[-1]] >= current_cost:
+      prev_index = stack.pop()
+      final_costs[prev_index] = costs[prev_index] - current_cost
+    stack.append(i)
+  return final_costs
+        
+
