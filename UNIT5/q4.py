09d2b570efc412ea1a4225f1d77c170794e36363
Anthony Paredes-Bautista <ap2357@cornell.edu>
2025-10-14 21:59:30 -0400

    q9 didnt finish

--- UNIT5/q4.py
+++ UNIT5/q4.py
@@ -1,12 +1,13 @@
 
 from collections import Counter
 class Villager:
-    def __init__(self, name, species, personality, catchphrase):
+    def __init__(self, name, species, personality, catchphrase, neighbor=None):
         self.name = name
         self.species = species
-        self.catchphrase = catchphrase
         self.personality = personality
+        self.catchphrase = catchphrase
         self.furniture = []
+        self.neighbor = neighbor
     
     def set_catchphrase(self, new_catchphrase):
         is_alphabetic_and_whitespace = all(c.isalpha() or c.isspace() for c in new_catchphrase)
@@ -69,3 +70,21 @@ def of_personality_type(townies, personality_type):
 # print(of_personality_type([isabelle, bob, stitches], "Lazy"))
 # print(of_personality_type([isabelle, bob, stitches], "Cranky"))
 
+def message_received(start_villager, target_villager):
+    curr = start_villager
+    while(curr):
+        if(curr.neighbor == target_villager):
+            return True
+        else:
+            curr = curr.neighbor
+    return False
+
+
+isabelle = Villager("Isabelle", "Dog", "Normal", "what's up?")
+tom_nook = Villager("Tom Nook", "Raccoon", "Cranky", "yes, yes")
+kk_slider = Villager("K.K. Slider", "Dog", "Lazy", "dig it")
+isabelle.neighbor = tom_nook
+tom_nook.neighbor = kk_slider
+
+print(message_received(isabelle, kk_slider))
+print(message_received(kk_slider, isabelle))
---
+++ UNIT5/q9.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+        
+        
+tom_nook = Node("Tom Nook")
+tommy = Node("Tommy") 
+tom_nook.next = tommy 
+print(tom_nook.value) 
+print(tom_nook.next.value) 
+print(tommy.value) 
+print(tommy.next) 
